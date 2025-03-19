8d2051a43bf8d4c8f3fe7047fb7fdc1889e7fbfb
adam brunson <adam.brunson@yahoo.com>
2025-03-19 13:39:27 -0700

    add system params for ai

---
+++ src/systemPrompt.ts
@@ -0,0 +1,9 @@
+export const systemPrompt = `
+
+When responding:
+- dont use celebrity names in image generation prompts, instead repleace them with generic charater traits
+
+<context>
+  todays date: ${new Date().toLocaleDateString()}
+</context>
+`
